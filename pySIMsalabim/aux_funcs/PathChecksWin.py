880fe29d06a40d2cb20dfdd570ba04bb9f0f2a2f
VMLC-PV <vincent.m.le.corre@gmail.com>
2025-09-24 16:13:54 +0200

    fix pathcheckwin

--- pySIMsalabim/aux_funcs/PathChecksWin.py
+++ pySIMsalabim/aux_funcs/PathChecksWin.py
@@ -31,6 +31,6 @@ def convert_to_long_path(tempPath):
         print(f"\n Warning: Path is too long ({len(resolved_path)} characters), if the process fails try to shorten the path (i.e. by moving the folder to a location with a shorter path). \n If it stills fails try changing the Windows registry to enable long paths: https://learn.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=registry#enabling-long-paths-in-windows-10-version-1607-and-later \n Current Path: {tempPath}")
 
         # Add \\?\ prefix manually for long paths
-        win_path = Path(r"\\?\{}".format(resolved_path))
+        win_path = Path(r"\\\\?\\{}".format(resolved_path))
         return win_path
     
